4b8e1b2536aa217784e50152ed49a2248aa00b40
yigitaldo <yigit.aldogan05@gmail.com>
2026-07-19 11:51:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-bot.getMe().then((me)=>{
-    console.log(me);
-});
 
-
-bot.sendMessage(
-    process.env.CHAT_ID,
-    "🚀 Test mesajı"
-)
-.then(()=>{
-    console.log("Mesaj gitti");
-})
-.catch(err=>{
-    console.log(err.message);
-});
